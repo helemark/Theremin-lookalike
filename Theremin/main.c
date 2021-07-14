e090a554df6f8b20f424f1c4ed609719872c15b7
Helene Markeng <Helene.hei@hotmail.com>
2021-07-14 09:35:09 +0200

    Pushing to my own Git for the first time <3

---
+++ Theremin/main.c
@@ -0,0 +1,27 @@
+/*
+ * Theremin.c
+ *
+ * Created: 14.07.2021 09.14.33
+ * Author : Helene
+ */ 
+
+#include <avr/io.h>
+
+#define F_CPU 20000000
+
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/twi.h>
+#include <util/delay.h>
+#include <avr/cpufunc.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
